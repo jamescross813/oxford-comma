46d19329992777e1cf5a9a724889ee4fa8f57a31
jamescross <crossjames813@gmail.com>
2021-01-20 20:29:49 -0600

    commit tree 430c9e50c18b4898cd3a1303b5dbf16363db70d6

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+ string =""
+  if
+    array.length == 1
+      array.join
+elsif array.length == 2
+     string << array.join( " and ")
+elsif array.length == 3
+      string << array[0..1].join(", ") 
+      string << ", and " + array[2]
+else array.length > 3
+    string << array[0..-2].join(", ")
+    string << ", and " + array[-1]
 end
+end
+
